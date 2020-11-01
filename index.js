5c73c561422c9576f926e412d7f5ce72db0faef7
BuildTools <unconfigured@null.spigotmc.org>
2020-11-01 11:18:50 +0900

    add push button and fetch

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-function click(){
+console.log('hogehoge');
+function clickBt(){
+    console.log('hogehoge');
     fetch('http://192.168.3.149/L',{mode: 'no-cors'})
     .then(response => response.text())
     .then(text => {
